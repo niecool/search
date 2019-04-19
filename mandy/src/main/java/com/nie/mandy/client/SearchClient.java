9fc8cb10fa8bd289b12580f6e3eaac0396bd62ee
linxuhong <weblogic_213R>
2019-04-19 15:35:54 +0800

    add jar

---
+++ mandy/src/main/java/com/nie/mandy/client/SearchClient.java
@@ -0,0 +1,11 @@
+package com.nie.mandy.client;
+
+/**
+ *
+ */
+public final class SearchClient {
+    private SearchClient() {
+    }
+
+
+}
---
+++ mandy/src/main/java/com/nie/mandy/client/com/nie/mandy/es/connection/EsHighClient.java
@@ -0,0 +1,18 @@
+package com.nie.mandy.client.com.nie.mandy.es.connection;
+
+public class EsHighClient {
+
+
+    private EsHighClient() {
+
+    }
+
+    /*
+         RestHighLevelClient client = new RestHighLevelClient(
+            RestClient.builder(
+                    new HttpHost("152.136.70.24", 9200, "http")));
+
+    */
+
+
+}
